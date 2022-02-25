2ef6905fbee87a6f3b574522ae89a4d1c84e4638
AshifShuvo <ashifzahanshuvo@gmail.com>
2022-02-25 10:34:35 +0600

    hmm

--- client/src/components/DocumentCard.js
+++ client/src/components/DocumentCard.js
@@ -1,11 +1,24 @@
-// import { useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function DocumentCard(){
+    const history = useHistory()
+
+    const goto_doc = () => {
+        history.push('/documents/a74f157d-bca3-4fb7-9424-e2d45a3f43b8')
+    }
     
     return (
         <div className="document-card">
             <h5 className="document-title">Document Title</h5>
+            <button 
+                className="button-primary" 
+                aria-label="LOGIN"
+                onClick={event =>  goto_doc()} //Login e click korle Home e niye jabe
+                >
+                Edit 
+            </button>
         </div>
+        
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,12 @@ import { useHistory } from "react-router-dom";
 import DocumentCard from "../components/DocumentCard";
 
 
-function Home(){    
+function Home(){   
+    const history = useHistory()
+
+    const goto_doc = () => {
+        history.push('/documents/a74f157d-bca3-4fb7-9424-e2d45a3f43b8')
+    } 
     return (
         <div className="page-container">
             <h1>Welcome Back Madarchod</h1>
